823f2f7662d4d36df6885bd32a148ea90430eec4
Deepak Divya Tejaswi <deepakdt159@gmail.com>
2024-02-22 05:35:43 +0000

    feat: add custom codes feature
    - user can now add custom codes and it will be stored in local storage

--- github-sonic-button.js
+++ github-sonic-button.js
@@ -11,25 +11,28 @@ function sonicButtonClick(event) {
 
 const parentDiv = document.getElementById("issue-comment-box");
 //const targetDiv = parentDiv.querySelector(".d-flex.flex-justify-end");
-const commentButtonTexts = ["RUN_SCA", "RUN_SYNTAX_CHECK", "RUN_UNIT_TEST", "RUN_FRONTEND_VALIDATION"];
 
+chrome.storage.local.get(['userMessage'], function (result) {
+    let commentButtonTexts = ["RUN_SCA", "RUN_UNIT_TESTS"];
+    if (result.userMessage) {
+        commentButtonTexts = result.userMessage.split(",").map(e => e.trim());
+    }
 
-const sonicButtonDiv = document.createElement("div");
-sonicButtonDiv.className = "d-flex ml-2 pl-7"; // Set class attribute
+    const sonicButtonDiv = document.createElement("div");
+    sonicButtonDiv.className = "d-flex ml-2 pl-7"; // Set class attribute
 
-for (let i = 0; i < commentButtonTexts.length; i++) {
-    const commentButtonText = commentButtonTexts[i]
-    const newDiv = document.createElement("div");
-    newDiv.className = "mr-1"; // Set class attribute
-    const newButton = document.createElement("button");
-    newButton.id = "sonic_sca_click";
-    newButton.type ="button";
-    newButton.className = "btn"; // Set class attribute
-    newButton.textContent = commentButtonText;
-    newButton.addEventListener("click", sonicButtonClick);
-    newDiv.appendChild(newButton);
-    sonicButtonDiv.appendChild(newDiv)
-}
-parentDiv.insertBefore(sonicButtonDiv, parentDiv.firstChild);
-
-
+    for (let i = 0; i < commentButtonTexts.length; i++) {
+        const commentButtonText = commentButtonTexts[i]
+        const newDiv = document.createElement("div");
+        newDiv.className = "mr-1"; // Set class attribute
+        const newButton = document.createElement("button");
+        newButton.id = "sonic_sca_click";
+        newButton.type ="button";
+        newButton.className = "btn"; // Set class attribute
+        newButton.textContent = commentButtonText;
+        newButton.addEventListener("click", sonicButtonClick);
+        newDiv.appendChild(newButton);
+        sonicButtonDiv.appendChild(newDiv)
+    }
+    parentDiv.insertBefore(sonicButtonDiv, parentDiv.firstChild);
+});
